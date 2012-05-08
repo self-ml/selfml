597e3bab9080460060c67af9ac11a8321f437a88
Ricardo Mendes <rokusu@gmail.com>
2012-05-08 19:39:59 +0100

    changed specs slightly and added basic_spec

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -1,16 +1,16 @@
 require_relative 'spec_helper'
 
 describe SelfML do
-  
-  subject { SelfML }
-  let(:fixtures) { load_fixture "strings" }
 
-  describe "Strings" do
-    
+  subject { SelfML }
+  let(:fixtures) { load_fixture "basic" }
+
+  describe "Basic" do
+
     it "parses example correctly" do
       subject.parse(fixtures[:input]).to_s.must_equal fixtures[:output]
     end
-    
+
   end
-  
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
+require 'bundler'
+Bundler.setup
+
 require 'minitest/autorun'
+
+require 'selfml'
 
 def load_fixture name
   {
-    :input  => File.read("fixtures/#{name}.selfml"),
-    :output => File.read("fixtures/output/#{name}.selfml")
+    :input  => File.read("spec/fixtures/#{name}.selfml"),
+    :output => File.read("spec/fixtures/output.#{name}.selfml")
   }
 end
