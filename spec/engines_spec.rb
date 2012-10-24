18a85eb341a190c01cdb28f541db4339dd4a2d3b
Ricardo Mendes <rokusu@gmail.com>
2012-10-24 14:31:37 +0100

    travis is helpful indeed

--- spec/engines_spec.rb
+++ spec/engines_spec.rb
@@ -1,3 +1,4 @@
+require_relative 'spec_helper'
 require 'selfml/engines/hash'
 
 describe SelfML do
@@ -5,15 +6,15 @@ describe SelfML do
   let(:document) { SelfML::Parser.new.parse("(grandparent (parent child1 child2))") }
 
   describe "Engines" do
-    
+
     describe "Hash" do
       subject      { SelfML::Engines::Hash.new }
       # let(:engine) { SelfML::Engines::Hash }
-      
+
       it "parses correctly" do
         subject.apply(document)
       end
-      
+
       it "outputs correctly" do
         output = {:document=>[{:grandparent=>[{:parent=>["child1", "child2"]}]}]}
         subject.apply(document).must_equal output
