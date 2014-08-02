b2e8e82471a4ab8078178483f90cbd6a46f4086a
Ricardo Mendes <rokusu@gmail.com>
2014-08-02 02:23:15 +0100

    whitespace

--- lib/selfml/engines/document.rb
+++ lib/selfml/engines/document.rb
@@ -8,31 +8,31 @@ module SelfML::Engines
   # The real deal.
   class Document < Parslet::Transform
     include SelfML::AST
-  
+
     rule(:document => sequence(:doc)) { SelfML::AST::Document.new(doc) }
     rule(:list => subtree(:l)) do
       Node.new(
         StringNode.new(l[:head]) , Array(l[:tail])
       )
     end
-  
+
     # Comments
     rule(:comment =>     simple(:s)) { Comment.new(s)             }
     rule(:block   =>   simple(:blk)) { Comment.new(blk)           }
     rule(:block   => sequence(:blk)) { Comment.new(blk.reduce :+) }
     rule(:line    =>    simple(:ln)) { Comment.new(ln)            }
-    
+
     # Strings
     rule(:string    => simple(:s)) { StringNode.new(s) }
     rule(:verbatim  => simple(:s)) { StringNode.new(s) }
-    
+
     rule(:backticks => sequence(:s)) { StringNode.new(s.reduce :+) }
     rule(:ticks     =>   simple(:s)) { "`" }
-    
+
     rule(:brackets => sequence(:b)) { StringNode.new(b.reduce :+) }
     rule(:nested   =>   simple(:n)) { StringNode.new(n) }
     rule(:nested   => sequence(:n)) { "[" + StringNode.new(n.reduce :+) + "]" }
-    
+
     rule(:text => simple(:t)) { StringNode.new(t) }
   end
 
--- lib/selfml/parser.rb
+++ lib/selfml/parser.rb
@@ -1,17 +1,17 @@
 require 'parslet'
 
 module SelfML
-  
+
   # Helper methods
   class Parslet::Parser
-    
+
     rule(:space)  { match('\s').repeat(1) }
     rule(:space?) { space.maybe }
-  
+
     rule(:word)   { match('\w').repeat }
-    rule(:word?)  { word.maybe }  
+    rule(:word?)  { word.maybe }
   end # monkey patching in a couple of helpers
-  
+
   # Class responsible for parsing all the things.
   #
   class Parser < Parslet::Parser
@@ -19,9 +19,9 @@ module SelfML
     rule(:top)  do
       (space? >> node >> space?).repeat.as(:document)
     end
-    
+
     rule(:node) { comment.as(:comment) | list.as(:list) | string.as(:string) }
-    
+
     # String
     rule(:string)    { backtick.as(:backticks) | bracketed.as(:brackets) | verbatim.as(:verbatim) }
     rule(:backtick) do
@@ -31,25 +31,25 @@ module SelfML
       ).repeat >> str('`')
     end
     rule(:bracketed) do
-    	str('[') >> (
+      str('[') >> (
         # bracketed.as(:nested) | match('[^\]]').as(:text)
         bracketed.as(:nested) | (str("]").absent? >> any).as(:text)
       ).repeat >> str(']')
     end
     rule(:verbatim)  { str("#").absnt? >> match['^\[\](){}\s'].repeat(1) }
-  
+
     # List
     rule(:tail) do
       (node >> space?).repeat
     end
     rule(:list) do
-    	str('(') >> space? >> 
-    	  comment.as(:comment).maybe >> space? >>
-    	  string.as(:head)           >> space? >>
-    	  tail.as(:tail)             >> space? >>
+      str('(') >> space? >>
+      comment.as(:comment).maybe >> space? >>
+      string.as(:head)           >> space? >>
+      tail.as(:tail)             >> space? >>
       space?   >> str(')')
     end
-    
+
     # Comment
     rule(:comment) { line.as(:line) | block.as(:block) }
     rule(:line)    { str('#') >> match['^\\n'].repeat.as(:text) >> (str("\n") | any.absent?) }
@@ -58,6 +58,6 @@ module SelfML
         block.as(:block) |  match['^#}'].as(:text)
       ).repeat >> str('#}')
     end
-  
+
   end
 end
